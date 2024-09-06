e68357619ba19bcb4345f204b5f18b7536d80b86
Albani Medina <albanimedina11@gmail.com>
2024-09-05 20:24:14 -0400

    Se elimina archivo holamundo.cpp

--- holamundo.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>  //indica al compilador que incluya las 
-                     //declaraciones de las funciones de E/S de 
-                     //flujo estándar que se encuentran en iostream
-int main()
-{
-std::cout << "Hola, Mundo!\n";
-}
